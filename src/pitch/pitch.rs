7b9bc5c9f98e0ce93f3135cd87174c1541f5f08a
Jesse Kuang <jkuang@21cn.com>
2022-07-10 09:04:33 +0800

    use ref for Message::Body

--- src/pitch/pitch.rs
+++ src/pitch/pitch.rs
@@ -83,8 +83,8 @@ pub fn from_bytes<'a>(buf: &'a [u8]) -> Result<Message<'a>> {
 
 pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
     let (index, tracking, timestamp) = (v.index, v.tracking, v.timestamp);
-    match v.body {
-        Body::SystemEvent(s) => {
+    match &v.body {
+        &Body::SystemEvent(s) => {
             let tag = b'S';
             let (event_code, time_hours) = (s.event as u8, s.time_hours);
             let src = SystemEventNet {
@@ -97,7 +97,7 @@ pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
             };
             ser_to_bytes(&src)
         }
-        Body::SymbolDirectory(s) => {
+        &Body::SymbolDirectory(s) => {
             let tag = b'R';
             let (
                 market_category,
@@ -138,7 +138,7 @@ pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
             };
             ser_to_bytes(&src)
         }
-        Body::TradingAction(s) => {
+        &Body::TradingAction(s) => {
             let tag = b'H';
             let (trading_state, reason) = (s.trading_state as u8, s.reason);
             let src = SymbolTradingActionNet {
@@ -151,7 +151,7 @@ pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
             };
             ser_to_bytes(&src)
         }
-        Body::AddOrder(s) => {
+        &Body::AddOrder(s) => {
             let tag = b'A';
             let (buy_sell, ref_no) = (s.side as u8, s.reference);
             let (qty, price) = (s.qty, s.price);
@@ -167,7 +167,7 @@ pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
             };
             ser_to_bytes(&src)
         }
-        Body::OrderExecuted(s) => {
+        &Body::OrderExecuted(s) => {
             let tag = b'E';
             let (printable, ref_no) = (s.printable, s.reference);
             let (qty, match_no) = (s.qty, s.match_no);
@@ -183,7 +183,7 @@ pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
             };
             ser_to_bytes(&src)
         }
-        Body::OrderExecutedWithPrice(s) => {
+        &Body::OrderExecutedWithPrice(s) => {
             let tag = b'C';
             let (printable, ref_no, qty, price, match_no) =
                 (s.printable, s.reference, s.qty, s.price, s.match_no);
@@ -200,7 +200,7 @@ pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
             };
             ser_to_bytes(&src)
         }
-        Body::OrderCancelled(s) => {
+        &Body::OrderCancelled(s) => {
             let tag = b'X';
             let cancel_reason = s.reason as u8;
             let (ref_no, qty) = (s.reference, s.cancelled);
@@ -215,7 +215,7 @@ pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
             };
             ser_to_bytes(&src)
         }
-        Body::OrderDelete(s) => {
+        &Body::OrderDelete(s) => {
             let tag = b'D';
             let (cancel_reason, ref_no) = (s.reason as u8, s.reference);
             let src = OrderDeleteNet {
@@ -228,7 +228,7 @@ pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
             };
             ser_to_bytes(&src)
         }
-        Body::ReplaceOrder(s) => {
+        &Body::ReplaceOrder(s) => {
             let tag = b'U';
             let (ref_no, new_ref_no, qty, price) =
                 (s.old_reference, s.new_reference, s.qty, s.price);
@@ -244,7 +244,7 @@ pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
             };
             ser_to_bytes(&src)
         }
-        Body::Trade(s) => {
+        &Body::Trade(s) => {
             let tag = b'P';
             let (buy_sell, ref_no, qty, price, match_no) =
                 (s.side as u8, s.reference, s.qty, s.price, s.match_no);
@@ -261,7 +261,7 @@ pub fn to_bytes<'a>(v: &'a Message) -> Result<Vec<u8>> {
             };
             ser_to_bytes(&src)
         }
-        Body::CrossTrade(s) => {
+        &Body::CrossTrade(s) => {
             let tag = b'Q';
             let (qty, price) = (s.qty, s.price);
             let (match_no, type_) = (s.match_no, s.cross_type as u8);
